20f50f0d0973d6ced433e391fe1fb04eca8641d6
Suzan97 <susannafula.sw@gmail.com>
2021-11-11 15:38:14 +0300

    Latest 1.5

--- VGS/settings.py
+++ VGS/settings.py
@@ -26,7 +26,7 @@ with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['127.0.0.1:8000', 'https://safe-dusk-87954.herokuapp.com/']
 ALLOWED_HOSTS = []
